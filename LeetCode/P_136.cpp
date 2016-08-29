12b3fd64d40709af6c4d607551d6c321db89ae8b
Rajarshi Biswas <rajarshibiswas25@gmail.com>
2016-08-28 23:15:14 -0400

    Update P_136.cpp

--- LeetCode/P_136.cpp
+++ LeetCode/P_136.cpp
@@ -8,7 +8,6 @@ Your algorithm should have a linear runtime complexity.
 Could you implement it without using extra memory?
 */
 
-
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
